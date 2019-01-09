a5acea7366c162332822b8bfc7baabef931636c6
thomsowell <35775843+thomsowell@users.noreply.github.com>
2019-01-09 08:20:11 -0500

    Add files via upload

---
+++ GroupBoxRadioControl/GroupBoxRadio.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace GroupBoxRadioControl
+{
+    partial class UserControl1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rb0 = new System.Windows.Forms.RadioButton();
+            this.rb1 = new System.Windows.Forms.RadioButton();
+            this.rb2 = new System.Windows.Forms.RadioButton();
+            this.rb3 = new System.Windows.Forms.RadioButton();
+            this.rb4 = new System.Windows.Forms.RadioButton();
+            this.rb5 = new System.Windows.Forms.RadioButton();
+            this.rb6 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rb6);
+            this.groupBox1.Controls.Add(this.rb5);
+            this.groupBox1.Controls.Add(this.rb4);
+            this.groupBox1.Controls.Add(this.rb3);
+            this.groupBox1.Controls.Add(this.rb2);
+            this.groupBox1.Controls.Add(this.rb1);
+            this.groupBox1.Controls.Add(this.rb0);
+            this.groupBox1.Location = new System.Drawing.Point(35, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1759, 100);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // rb0
+            // 
+            this.rb0.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb0.AutoSize = true;
+            this.rb0.Location = new System.Drawing.Point(23, 47);
+            this.rb0.Name = "rb0";
+            this.rb0.Size = new System.Drawing.Size(144, 35);
+            this.rb0.TabIndex = 0;
+            this.rb0.TabStop = true;
+            this.rb0.Text = "radioButton1";
+            this.rb0.UseVisualStyleBackColor = true;
+            // 
+            // rb1
+            // 
+            this.rb1.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb1.AutoSize = true;
+            this.rb1.Location = new System.Drawing.Point(207, 47);
+            this.rb1.Name = "rb1";
+            this.rb1.Size = new System.Drawing.Size(144, 35);
+            this.rb1.TabIndex = 1;
+            this.rb1.TabStop = true;
+            this.rb1.Text = "radioButton2";
+            this.rb1.UseVisualStyleBackColor = true;
+            // 
+            // rb2
+            // 
+            this.rb2.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb2.AutoSize = true;
+            this.rb2.Location = new System.Drawing.Point(391, 47);
+            this.rb2.Name = "rb2";
+            this.rb2.Size = new System.Drawing.Size(144, 35);
+            this.rb2.TabIndex = 2;
+            this.rb2.TabStop = true;
+            this.rb2.Text = "radioButton3";
+            this.rb2.UseVisualStyleBackColor = true;
+            // 
+            // rb3
+            // 
+            this.rb3.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb3.AutoSize = true;
+            this.rb3.Location = new System.Drawing.Point(575, 47);
+            this.rb3.Name = "rb3";
+            this.rb3.Size = new System.Drawing.Size(144, 35);
+            this.rb3.TabIndex = 3;
+            this.rb3.TabStop = true;
+            this.rb3.Text = "radioButton4";
+            this.rb3.UseVisualStyleBackColor = true;
+            // 
+            // rb4
+            // 
+            this.rb4.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb4.AutoSize = true;
+            this.rb4.Location = new System.Drawing.Point(759, 47);
+            this.rb4.Name = "rb4";
+            this.rb4.Size = new System.Drawing.Size(144, 35);
+            this.rb4.TabIndex = 4;
+            this.rb4.TabStop = true;
+            this.rb4.Text = "radioButton5";
+            this.rb4.UseVisualStyleBackColor = true;
+            // 
+            // rb5
+            // 
+            this.rb5.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb5.AutoSize = true;
+            this.rb5.Location = new System.Drawing.Point(943, 47);
+            this.rb5.Name = "rb5";
+            this.rb5.Size = new System.Drawing.Size(144, 35);
+            this.rb5.TabIndex = 5;
+            this.rb5.TabStop = true;
+            this.rb5.Text = "radioButton6";
+            this.rb5.UseVisualStyleBackColor = true;
+            // 
+            // rb6
+            // 
+            this.rb6.Appearance = System.Windows.Forms.Appearance.Button;
+            this.rb6.AutoSize = true;
+            this.rb6.Location = new System.Drawing.Point(1127, 47);
+            this.rb6.Name = "rb6";
+            this.rb6.Size = new System.Drawing.Size(144, 35);
+            this.rb6.TabIndex = 6;
+            this.rb6.TabStop = true;
+            this.rb6.Text = "radioButton7";
+            this.rb6.UseVisualStyleBackColor = true;
+            // 
+            // UserControl1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.groupBox1);
+            this.Name = "UserControl1";
+            this.Size = new System.Drawing.Size(1841, 450);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rb6;
+        private System.Windows.Forms.RadioButton rb5;
+        private System.Windows.Forms.RadioButton rb4;
+        private System.Windows.Forms.RadioButton rb3;
+        private System.Windows.Forms.RadioButton rb2;
+        private System.Windows.Forms.RadioButton rb1;
+        private System.Windows.Forms.RadioButton rb0;
+    }
+}
---
+++ GroupBoxRadioControl/GroupBoxRadio.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GroupBoxRadioControl
+{
+    public partial class UserControl1: UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+
+            List<RadioButton> rbList = new List<RadioButton>();
+
+            rbList.Add(this.rb0);
+            rbList.Add(this.rb1);
+            rbList.Add(this.rb2);
+            rbList.Add(this.rb3);
+            rbList.Add(this.rb4);
+            rbList.Add(this.rb5);
+            rbList.Add(this.rb6);
+
+
+        }
+
+        public int CurrentSelection(RadioButton rb)
+        {
+        https://docs.microsoft.com/en-us/previous-versions/dotnet/articles/ms996431(v=msdn.10)
+            int currentValue = 0;
+            {
+                if ( rb.Checked > 0 & Value <= )
+                    radioButtons(Value - 1).Checked = true;
+                rb = 5;
+            }
+        }
+    }
+}
